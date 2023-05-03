29cde6e35d8420a3565df5171d891f8d03ac5140
monty <patrick@montycasa.com>
2023-05-02 22:26:00 -0400

    Setup fixtures to add all test data within the golf_contest conftest.py

--- golf_contest/tests/conftest.py
+++ golf_contest/tests/conftest.py
@@ -1,9 +1,131 @@
+import datetime
+
 import pytest
-from django.core.management import call_command
+
+from golf_contest.models import Golfer, Team, Tournament
+from mysite.users.models import User
+
+# from django.core.management import call_command
 
 
-@pytest.fixture(scope="session")
-def django_db_setup(django_db_setup, django_db_blocker):
-    with django_db_blocker.unblock():
-        call_command("loaddata", "golf_contest_data.json")
-        # call_command('loaddata', 'user_data.json')
+GOLFERS = [
+    {
+        "name": "Tiger Woods",
+        "tournament_position": 1,
+        "score_to_par": -18,
+    },
+    {
+        "name": "Tom Kite",
+        "tournament_position": 2,
+        "score_to_par": -6,
+    },
+    {
+        "name": "Tommy Tolles",
+        "tournament_position": 3,
+        "score_to_par": -5,
+    },
+    {
+        "name": "Tom Watson",
+        "tournament_position": 4,
+        "score_to_par": -4,
+    },
+    {
+        "name": "Costantino Rocca",
+        "tournament_position": 5,
+        "score_to_par": -3,
+    },
+    {
+        "name": "Paul Stankowski",
+        "tournament_position": 5,
+        "score_to_par": -3,
+    },
+    {
+        "name": "Fred Couples",
+        "tournament_position": 7,
+        "score_to_par": -2,
+    },
+    {
+        "name": "Bernhard Langer",
+        "tournament_position": 7,
+        "score_to_par": -2,
+    },
+    {
+        "name": "Justin Leonard",
+        "tournament_position": 7,
+        "score_to_par": -2,
+    },
+    {
+        "name": "Davis Love III",
+        "tournament_position": 7,
+        "score_to_par": -2,
+    },
+    {
+        "name": "Jeff Sluman",
+        "tournament_position": 7,
+        "score_to_par": -2,
+    },
+]
+
+
+@pytest.fixture
+def tournament_data(db):
+    return Tournament.objects.create(
+        name="Masters", start_date=datetime.date(year=2023, month=4, day=6), state="FNL", world_ranking_week=0
+    )
+
+
+@pytest.fixture
+def golfer_data(db, tournament_data):
+    data = []
+    for golfer in GOLFERS:
+        data.append(
+            Golfer.objects.create(
+                name=golfer["name"],
+                tournament_position=golfer["tournament_position"],
+                score_to_par=golfer["score_to_par"],
+                tournament=tournament_data,
+            )
+        )
+    return data
+
+
+@pytest.fixture
+def user_data(db):
+    user1 = User.objects.create(email="user1@test.com")
+    user1.set_password("password")
+    user1.save()
+    user2 = User.objects.create(email="user2@test.com")
+    user2.set_password("password")
+    user2.save()
+
+    return [user1, user2]
+
+
+@pytest.fixture
+def team_data(user_data, tournament_data, golfer_data):
+    team1 = Team.objects.create(name="Pimento Cheese", user=user_data[0], tournament=tournament_data)
+
+    team1.add_golfer(golfer_data[1])
+    team1.add_golfer(golfer_data[3])
+    team1.add_golfer(golfer_data[5])
+    team1.add_golfer(golfer_data[7])
+    team1.add_golfer(golfer_data[9])
+    team1.save()
+
+    team2 = Team.objects.create(name="Maester Aemon Corner", user=user_data[1], tournament=tournament_data)
+
+    team2.add_golfer(golfer_data[2])
+    team2.add_golfer(golfer_data[4])
+    team2.add_golfer(golfer_data[6])
+    team2.add_golfer(golfer_data[8])
+    team2.add_golfer(golfer_data[10])
+    team2.save()
+
+    return [team1, team2]
+
+
+# @pytest.fixture(scope="session")
+# def django_db_setup(django_db_setup, django_db_blocker):
+#     with django_db_blocker.unblock():
+#         call_command("loaddata", "golf_contest_data.json")
+# call_command('loaddata', 'user_data.json')
--- golf_contest/tests/test_models.py
+++ golf_contest/tests/test_models.py
@@ -1,91 +1,91 @@
 import datetime
 
-import pytest
-
 from golf_contest.models import Golfer, Team, Tournament
-from mysite.users.models import User
+
+# @pytest.fixture
+# def new_tournament(db) -> Tournament:
+#     return Tournament.objects.create(name="Philly", start_date=datetime.date(year=2023, month=4, day=6))
 
 
-@pytest.fixture
-def new_tournament(db) -> Tournament:
-    return Tournament.objects.create(name="Philly", start_date=datetime.date(year=2023, month=4, day=6))
+# @pytest.fixture
+# def new_golfer(db, new_tournament) -> Golfer:
+#     return Golfer.objects.create(name="Tiger Woods", tournament=new_tournament)
 
 
-@pytest.fixture
-def new_golfer(db, new_tournament) -> Golfer:
-    return Golfer.objects.create(name="Tiger Woods", tournament=new_tournament)
+# @pytest.fixture
+# def new_team(db) -> Team:
+#     # TODO Need to create test users somehow.
+#     user1 = User.objects.get(pk=1)
+#     team = Team.objects.create(name="Pimento Cheese", user=user1)
+#     team.tournament = Tournament.objects.get(pk=1)
+
+#     for i in range(5):
+#         team.add_golfer(Golfer.objects.get(pk=1))
+#     team.save()
 
 
-@pytest.fixture
-def new_team(db) -> Team:
-    # TODO Need to create test users somehow.
-    user1 = User.objects.get(pk=1)
-    team = Team.objects.create(name="Pimento Cheese", user=user1)
-    team.tournament = Tournament.objects.get(pk=1)
-
-    for i in range(5):
-        team.add_golfer(Golfer.objects.get(pk=1))
-    team.save()
-
-
-def test_golfer_create(new_golfer):
+def test_golfer_create(golfer_data):
     assert Golfer.objects.count() > 0
 
 
-def test_golfer_filter(new_golfer):
+def test_golfer_filter(golfer_data):
     assert Golfer.objects.filter(name="Tiger Woods").exists()
 
 
-def test_golfer_update(new_golfer):
-    new_golfer.name = "Smiley Kaufman"
-    new_golfer.save()
+def test_golfer_update(golfer_data):
+    my_golfer = Golfer.objects.get(name="Fred Couples")
+    my_golfer.name = "Smiley Kaufman"
+    my_golfer.save()
 
     golfer_from_db = Golfer.objects.get(name="Smiley Kaufman")
     assert golfer_from_db.name == "Smiley Kaufman"
 
 
-def test_golfer_tee_time_blank(new_golfer):
-    assert new_golfer.rd_one_tee_time is None
+def test_golfer_tee_time_blank(golfer_data):
+    my_golfer = Golfer.objects.get(name="Fred Couples")
+    assert my_golfer.rd_one_tee_time is None
 
 
-def test_tournament_contains_golfer(new_golfer, new_tournament):
-    assert new_tournament.golfer_set.filter(pk=new_golfer.pk).exists()
+def test_tournament_contains_golfer(golfer_data, tournament_data):
+    my_golfer = Golfer.objects.get(name="Fred Couples")
+    assert tournament_data.golfer_set.filter(name=my_golfer.name).exists()
 
 
-def test_golfer_references_tournament(new_golfer, new_tournament):
-    assert new_golfer.tournament == new_tournament
+def test_golfer_references_tournament(golfer_data, tournament_data):
+    my_golfer = Golfer.objects.get(name="Fred Couples")
+    assert my_golfer.tournament == tournament_data
 
 
-def test_tournament_create(new_tournament):
+def test_tournament_create(tournament_data):
     assert Tournament.objects.count() > 0
-    assert new_tournament.name == "Philly"
-    assert new_tournament.start_date == datetime.date(year=2023, month=4, day=6)
+    assert tournament_data.name == "Masters"
+    assert tournament_data.start_date == datetime.date(year=2023, month=4, day=6)
 
 
-def test_tournament_filter(new_tournament):
-    assert Tournament.objects.filter(name="Philly").exists()
+def test_tournament_filter(tournament_data):
+    assert Tournament.objects.filter(name="Masters").exists()
 
 
-def test_tournament_update(new_tournament):
-    new_tournament.name = "PGA Championship"
-    new_tournament.save()
+def test_tournament_update(tournament_data):
+    tournament_data.name = "PGA Championship"
+    tournament_data.save()
 
     tournament_from_db = Tournament.objects.get(name="PGA Championship")
     assert tournament_from_db.name == "PGA Championship"
 
 
-def test_tournament_year_property(new_tournament):
-    start_date_year = new_tournament.start_date.year
-    assert new_tournament.year == start_date_year
+def test_tournament_year_property(tournament_data):
+    start_date_year = tournament_data.start_date.year
+    assert tournament_data.year == start_date_year
 
 
-def test_team_create(new_team):
+def test_team_create(team_data):
     assert Team.objects.count() > 0
 
 
-def test_team_update(new_team):
-    new_team.name = "Maester Aemon Corner"
-    new_team.save()
+def test_team_update(team_data):
+    team_data[0].name = "Top Golf"
+    team_data[0].save()
 
-    team_from_db = Team.objects.get(name="Maester Aemon Corner")
-    assert team_from_db.name == "Maester Aemon Corner"
+    team_from_db = Team.objects.get(name="Top Golf")
+    assert team_from_db.name == "Top Golf"
