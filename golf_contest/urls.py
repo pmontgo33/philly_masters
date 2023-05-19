b19ec55fc3e9e893584bedfe70238111c34fb1a9
monty <patrick@montycasa.com>
2023-05-18 21:54:05 -0400

    Started new team form

--- golf_contest/urls.py
+++ golf_contest/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("tournament/<int:pk>/", views.standings, name="standings"),
     path("leaderboard/<int:pk>/", views.leaderboard, name="leaderboard"),
+    path("team/new", views.team_new, name="team_new"),
 ]
--- golf_contest/views.py
+++ golf_contest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from .forms import TeamForm
 from .models import Tournament
 
 
@@ -10,12 +11,15 @@ def index(request):
 def standings(request, pk):
     tournament = Tournament.objects.get(pk=pk)
     teams = tournament.team_set.all()
-
     return render(request, "golf_contest/standings.html", {"tournament": tournament, "teams": teams})
 
 
 def leaderboard(request, pk):
     tournament = Tournament.objects.get(pk=pk)
     golfers = tournament.golfer_set.all()
-
     return render(request, "golf_contest/leaderboard.html", {"tournament": tournament, "golfers": golfers})
+
+
+def team_new(request):
+    form = TeamForm()
+    return render(request, "golf_contest/team_new.html", {"form": form})
